a34569c8e6018e3b6ff750e6d7e86cd06a2e71f3
kimorak184 <kimorak184@gmail.com>
2021-01-15 20:10:43 +0100

    Create wbm.php

---
+++ licmembers/wbm.php
@@ -0,0 +1,1 @@
+{"IsExsist":true,"ErrorDescription":"NO","Response":{"ExpiryDate":"29991231","Name":"KABILLO","Email":"kabillo@nullnoss.org","Mobile":"000-00-00-00-00","Status":1,"AllowSending":true,"AllowBot":true,"AllowFilter":true,"RequestKey":"OK"}}
